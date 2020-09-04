c59de72e5cce7b06ebfcc612e200b7fa3416e858
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2020-09-04 15:01:38 -0400

    fix routes and methods

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import json
 from flask import Flask, request, jsonify, render_template
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_cors import CORS
-from models import db
+from models import db, Contact
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -15,6 +16,81 @@ db.init_app(app)
 Migrate(app, db)
 manager = Manager(app)
 manager.add_command("db", MigrateCommand)
+
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+
+@app.route("/api/contacts", methods=['GET', 'POST'])
+@app.route("/api/contacts/<int:id>", methods=['GET', 'PUT', 'DELETE'])
+@app.route("/api/contacts/name/<name>", methods=['GET'])
+def contacts(id = None, name = None):
+    if request.method == 'GET':
+        if name is not None:
+            contact = Contact.query.filter_by(name="Luis Rodriguez").first()
+            contacts = Contact.query.filter(Contact.name.like(""+name+"%")).all()
+            print(contacts)
+            #return jsonify(conacts), 200
+        elif id is not None:
+            contact = Contact.query.get(id)
+            if contact:
+                return jsonify(contact.serialize()), 200
+            else:
+                return jsonify({"msg": "Contact not found"}), 404
+        else:
+            contacts = Contact.query.all()
+            contacts = list(map(lambda contact: contact.serialize(), contacts))
+            return jsonify(contacts), 200
+
+
+    if request.method == 'POST':
+        name = request.json.get("name", None)
+        phone = request.json.get("phone", None)
+
+        if not name:
+            return jsonify({"msg": "Name is required"}), 400
+        if not phone:
+            return jsonify({"msg": "Phone is required"}), 400
+
+        contact = Contact()
+        contact.name = name
+        contact.phone = json.dumps(phone)
+        contact.save()
+
+        return jsonify(contact.serialize()), 201
+        
+
+    if request.method == 'PUT':
+        name = request.json.get("name", None)
+        phone = request.json.get("phone", None)
+
+        if not name:
+            return jsonify({"msg": "Name is required"}), 400
+        if not phone:
+            return jsonify({"msg": "Phone is required"}), 400
+
+        contact = Contact.query.get(id)
+
+        if not contact:
+            return jsonify({"msg": "Contact not found"}), 404
+
+        contact.name = name
+        contact.phone = json.dumps(phone)
+        contact.update()
+
+        return jsonify(contact.serialize()), 200
+
+    if request.method == 'DELETE':
+        contact = Contact.query.get(id)
+
+        if not contact:
+            return jsonify({"msg": "Contact not found"}), 404
+
+        contact.delete()
+
+        return jsonify({"success": "Contact was deleted"}), 200
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,2 +1,27 @@
+import json
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+class Contact(db.Model):
+    __tablename__ = 'contacts'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "phone": json.loads(self.phone)
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
